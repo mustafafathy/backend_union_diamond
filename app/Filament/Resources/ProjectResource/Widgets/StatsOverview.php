d99336d3477a47b5bb285f44b47004e4fef54ed7
mustafafathy <45427497+mustafafathy@users.noreply.github.com>
2024-04-28 03:05:03 +0300

    Update StatsOverview.php

--- app/Filament/Resources/ProjectResource/Widgets/StatsOverview.php
+++ app/Filament/Resources/ProjectResource/Widgets/StatsOverview.php
@@ -24,14 +24,14 @@ class StatsOverview extends BaseWidget
 
         return [
             Stat::make('Projects', $project->count())
-                ->icon('heroicon-O-globe-alt')
+                ->icon('heroicon-o-list-bullet')
                 ->description($groupedProjects[count($groupedProjects) - 1] . ' This Month')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart($groupedProjects)
                 ->color('success'),
 
             Stat::make('Units', $project->sum('units_no'))
-                ->icon('heroicon-O-home-modern')
+                ->icon('heroicon-o-list-bullet')
                 ->description($groupedUnits[count($groupedUnits) - 1] . ' This Month')
                 ->descriptionIcon('heroicon-m-arrow-trending-up')
                 ->chart($groupedUnits)
