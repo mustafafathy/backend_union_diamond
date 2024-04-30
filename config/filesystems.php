dddae1f0876cd42f9d76d2681371e399aff51154
mustafafathy <45427497+mustafafathy@users.noreply.github.com>
2024-04-30 22:31:14 +0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,8 +38,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => public_path('storage/'),
-            'url' => env('APP_URL') . 'public/storage/',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/public/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
